9625dbc079f0b6852fc9ac3b35e97f0be83c3997
Vũ Phương <vuvietphuonggm@gmail.com>
2019-12-09 22:54:59 +0700

    Thêm chức năng sửa, xóa với hàng hóa

---
+++ WebBanMayTinh2/WebBanMayTinh2/Models/AdminViewModels/SanPhamViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanMayTinh.Models;
+namespace WebBanMayTinh.Models.AdminViewModels
+{
+    public class SanPhamViewModel
+    {
+        public SelectList loaiSanPhams;
+        public SelectList coSos;
+        public SelectList trangThais;
+        public SanPham sanPham;
+    }
+}
